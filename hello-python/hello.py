4f116fbf7f60fd363d4b057a82248dba1dec8ddf
Andrew Farrell <andrew.farrell@anz.com>
2016-08-10 22:35:15 +1000

    adsadf

--- hello-python/hello.py
+++ hello-python/hello.py
@@ -5,31 +5,33 @@ from flask import Flask
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
-#BLUE = "#0099FF"
-#GREEN = "#33CC33"
+BLUE = "#0099FF"
+GREEN = "#33CC33"
 
-COLOR = "#000000"
+COLOR = BLUE
 counter = 0 
 
 
 @app.route('/')
 def hello():
     global counter
-    global color
     counter += 1
+    if counter %2 == 0:
+        COLOR = GREEN
+    else:
+        COLOR = BLUE
+    
+    return """
     <html>
     <body bgcolor="{}">
-
-    <center><h1><font color="pink">Hi, I'm Andrew Farrell's awesome app":<br/>
+    <center><h1><font color="pink">Hi, I'm GUID:<br/>
     {}</br>
     
     <br>
     
     Page Hit Count {}
-
     </center>
     <br>
-
     </body>
     </html>
     """.format(COLOR,my_uuid,counter)
